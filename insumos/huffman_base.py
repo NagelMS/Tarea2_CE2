7c7fe27a70ddd063e17e94b67f8cefc4aaf1cfc6
NagelMS <nagelmese@gmail.com>
2025-04-11 19:42:11 -0600

    Avance parte 1 - Documentación de código original - Calculos de longitudes, varianza y eficiencias

--- insumos/huffman_base.py
+++ insumos/huffman_base.py
@@ -32,10 +32,12 @@ def myfunc(argv):
 if __name__ == "__main__":
     myfunc(sys.argv)
 
+# Definición de archivos posteriores
 
 file_huffman_comprimido = file_full_path+".huffman"
 ruta_diccionario = file_full_path+".diccionario.csv"
 recovered_path = os.path.join(file_split_path[0], "recovered_"+file_split_path[1]);
+
 #-----------------------------------------------------
 # Algorithmo de compresión de huffman
 #-----------------------------------------------------
@@ -84,41 +86,88 @@ def insert_in_tree(raiz, ruta, valor):
 def huffman_code_tree(node, left=True, binString=''):
     if type(node) is int:
         return {node: binString}
+    # Se asigna a l el hijo izquierdo y r el hijo derecho del nodo
     (l, r) = node.children()
     d = dict()
+    # Se actualiza el código en el arbol
     d.update(huffman_code_tree(l, True, binString + '0'))
     d.update(huffman_code_tree(r, False, binString + '1'))
     return d
     
 
-# calculo de frecuencias y probabilidades
+# Calculo de frecuencias y probabilidades
 prob_unit = 1/len(string)
+# Tupla de frecuencias: Guarda el caracter y la probabilidad.
 freq = {}
 for c in string:
+    #print("Char",c)
     if c in freq:
         freq[c] += prob_unit
     else:
         freq[c] = prob_unit
 
+# Ordena los códigos según la probabilidad, de forma descendente
 freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
 
+# Se crea una tupla nodes igual a freq
 nodes = freq
 
+# Mientras que el largo de nodes sea mayor a 1
 while len(nodes) > 1:
+    # Se asignan los valores de las menores probabilidades
     (key1, c1) = nodes[-1]
     (key2, c2) = nodes[-2]
+    # nodes se convierte en una tupla más pequeña
     nodes = nodes[:-2]
+    # Se genera un node los hijos de menor probabilidad
     node = NodeTree(key1, key2)
+    # Se añada a la tupla el nodo generado con la suma de las dos probabilidades
     nodes.append((node, c1 + c2))
     #print(nodes)
+    # Se ordenan de forma descendente
     nodes = sorted(nodes, key=lambda x: x[1], reverse=True)
 
 huffmanCode = huffman_code_tree(nodes[0][0])
 
+# Imprimiendo códigos generados
 print(' Char | Huffman code ')
 print('----------------------')
 for (char, frequency) in freq:
     print(' %-4r |%12s' % (char, huffmanCode[char]))
 
+# Calculo de la entropía de la fuente
 
+entropy = 0
 
+for _,p in freq:
+    entropy -=  p*log2(p)
+
+print("\nEntropía de la fuente:", entropy ,"bits/símbolo")
+
+# Longitud media del código generado
+
+len_media = 0
+
+for (char,p) in freq:
+    len_media += p*len(huffmanCode[char])
+
+print("\nLongitud media del código generado:", len_media ,"bits/símbolo")
+
+# Varianza 
+
+varianza = 0
+
+for (char, p) in freq:
+    longitud = len(huffmanCode[char])
+    varianza += p * (longitud - len_media) ** 2
+
+print("\nVarianza de la longitud del código:", varianza)
+
+# Calculo de eficiencias
+
+eff_orig = entropy/8; # Se divide entre 8 ya que originalmente cada simbolo estaba representado por 8 bits
+
+eff_huffman = entropy/len_media; 
+
+print("\nEficiencia del código original:", eff_orig)
+print("\nEficiencia del código de huffman:", eff_huffman)
